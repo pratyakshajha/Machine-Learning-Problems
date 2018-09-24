6a85af85099d100c498ff0e82ba0f46d43b88391
Pratyaksha Jha <34786069+pratyakshajha@users.noreply.github.com>
2018-09-25 02:35:34 +0530

    Add files via upload

--- Iris Dataset/Iris dataset.py
+++ Iris Dataset/Iris dataset.py
@@ -5,15 +5,18 @@ import numpy as np
 from sklearn.metrics import precision_score
 from sklearn.externals import joblib
 
+# Load data
 iris = datasets.load_iris()
 X = iris.data
 y = iris.target
 
+# Split into train - test sets
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state=42)
 
 dtrain = xgb.DMatrix(X_train, label = y_train)
 dtest = xgb.DMatrix(X_test, label = y_test)
 
+# Set parameters
 param = {
     'max_depth': 3,  # max depth of a tree
     'eta': 0.3,      # training step for an iteration
@@ -23,8 +26,10 @@ param = {
 }
 num_round = 30       # number of iterations
 
+# Train
 mdl = xgb.train(param, dtrain, num_round)
 
+# Evaluate
 preds = mdl.predict(dtest)
 print(preds)
 
@@ -32,9 +37,12 @@ best_preds = np.asarray([np.argmax(val) for val in preds])
 
 print(precision_score(y_test, best_preds, average = 'macro'))
 
+# Save model
 joblib.dump(mdl, 'iris_model.pkl', compress = True)
 
+# Load saved model
 mdl_loaded = joblib.load('iris_model.pkl')
 
+# Check whether loaded model gives same result as actual one
 print((mdl_loaded.predict(dtest) == preds).all())
 
